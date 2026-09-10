2f4a9a1ffa949623e6998e84b54d50be235db419
MinSup Kim <74331678+br4c3@users.noreply.github.com>
2026-09-10 09:35:05 +0900

    [Passed] 합성수 판별기 (1ms, 0MB)

---
+++ trail1/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 2; i <= N-1; ++i)
+    {
+        if (N % i == 0)
+        {
+            printf("C");
+            return 0;
+        }
+    }
+    printf("N");
+
+    return 0;
+}
